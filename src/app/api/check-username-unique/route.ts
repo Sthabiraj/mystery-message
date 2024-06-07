14b65ca34ceac161d2948df7ef787f919cdb4aaa
Sthabiraj <birajshrestha2007@gmail.com>
2024-06-07 10:10:44 +0545

    ✨ api: Created api for unique username and code verification

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,77 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { z } from "zod";
+
+const UserNameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+    // validate with zod
+    const result = UserNameQuerySchema.safeParse(queryParam);
+    console.log(result); // TODO: remove this line
+    if (!result.success) {
+      const usernameError = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameError?.length > 0
+              ? usernameError.join(", ")
+              : "Invalid query parameter",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Error checking username", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
